7d5a73ffbb1fe3a43bfcd3a2724f4db4b7c4294d
Alejo Avenali <alejoavenali@gmail.com>
2023-11-26 23:45:22 -0300

    fix type

--- src/components/Forms/Forms.tsx
+++ src/components/Forms/Forms.tsx
@@ -1,11 +1,15 @@
-import React, { useLayoutEffect } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import Links from "../Links/Links";
 import { FormsStyle } from "./Forms.style";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Profile from "../Profile/Profile";
 
-const Forms = ({ setPreview }) => {
+type Props = {
+  setPreview: Dispatch<SetStateAction<boolean>>;
+};
+
+const Forms = ({ setPreview }: Props) => {
   return (
     <FormsStyle className='forms-wrapper'>
       <Swiper
